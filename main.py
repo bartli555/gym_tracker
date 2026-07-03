dc610c679602167de213defa9d6c37b86bdf8a9b
bartli555 <barlis3456@gmail.com>
2026-07-03 15:33:27 +0200

    Dodano zapis wagi

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import json
 
+class BodyWeightLog:
+    def __init__(self, date, weight, notes=""):
+        self.date = date
+        self.weight = weight
+        self.notes = notes
+
+    def show_info(self):
+        print(f"[{self.date}] Waga: {self.weight}kg | Notatki: {self.notes}")
+
+    def save_to_json(self):
+        # 1. Pakujemy pomiar w słownik
+        weight_data = {
+                "date": self.date,
+                "weight": self.weight,
+                "notes": self.notes
+            }
+
+            # 2. Tworzymy nazwę pliku (inną niż dla treningu, żeby zachować porządek)
+        filename = f"waga_{self.date}.json"
+
+            # 3. Zapis na dysk
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(weight_data, file, indent=4, ensure_ascii=False)
+
+        print(f"Zapisano wagę {self.weight} w pliku {filename}")
+        pass
+
+
+
 class TrainingSet:
     # To jest konstruktor - odpala się przy tworzeniu każdej nowej serii
     def __init__(self, exercise_name, weight, reps):
@@ -95,3 +124,10 @@ dzisiejszy_trening.save_to_json()
 # 3. Odpalamy Twoją nową funkcję liczącą sumę
 print(f"PODSUMOWANIE:")
 print(f"Całkowity tonaż dzisiejszej sesji to: {dzisiejszy_trening.calculate_total_volume()} kg")
+
+# Tworzymy nowy pomiar
+dzisiejsza_waga = BodyWeightLog("2026-07-03", 92.5, "Redukcja idzie zgodnie z planem")
+
+# Wyświetlamy i zapisujemy
+dzisiejsza_waga.show_info()
+dzisiejsza_waga.save_to_json()
