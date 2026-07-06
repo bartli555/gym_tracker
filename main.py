0ee22eb8830541ae41253f5780cba94467f36a07
bartli555 <barlis3456@gmail.com>
2026-07-06 16:20:01 +0200

    Dodano menu

--- main.py
+++ main.py
@@ -57,17 +57,17 @@ class TrainingSet:
 # --- TESTOWANIE W PRAKTYCE ---
 
 # Tworzymy konkretne obiekty (Twoje serie)
-set_1 = TrainingSet("Bench Press", 80, 8)
-set_2 = TrainingSet("Front Squat", 100, 6)
-set_3 = TrainingSet("RDL", 120, 4)
+#set_1 = TrainingSet("Bench Press", 80, 8)
+#set_2 = TrainingSet("Front Squat", 100, 6)
+#set_3 = TrainingSet("RDL", 120, 4)
 
 # Odpalamy gotową metodę
-set_1.show_info()
-set_2.show_info()
-set_3.show_info()
+#set_1.show_info()
+#set_2.show_info()
+#set_3.show_info()
 
 # Testujemy Twoją metodę calculate_volume()
-print(f"Objętość RDL: {set_3.calculate_volume()} kg")
+#print(f"Objętość RDL: {set_3.calculate_volume()} kg")
 
 # -----------------------------------------------------
 
@@ -128,23 +128,115 @@ class Workout:
 
 # --- TESTOWANIE KOMPOZYCJI ---
 # 1. Tworzymy nowy trening
-dzisiejszy_trening = Workout("2026-07-02", "Nogi & Push")
+#dzisiejszy_trening = Workout("2026-07-02", "Nogi & Push")
 
 # 2. Dorzucamy serie (obiekty set_1, set_2, set_3 stworzyliśmy w poprzednim zadaniu)
-dzisiejszy_trening.add_set(set_1)
-dzisiejszy_trening.add_set(set_2)
-dzisiejszy_trening.add_set(set_3)
+#dzisiejszy_trening.add_set(set_1)
+#dzisiejszy_trening.add_set(set_2)
+#dzisiejszy_trening.add_set(set_3)
 
 # Zapisujemy nasz trening do pliku!
-dzisiejszy_trening.save_to_json()
+#dzisiejszy_trening.save_to_json()
 
 # 3. Odpalamy Twoją nową funkcję liczącą sumę
-print(f"PODSUMOWANIE:")
-print(f"Całkowity tonaż dzisiejszej sesji to: {dzisiejszy_trening.calculate_total_volume()} kg")
+#print(f"PODSUMOWANIE:")
+#print(f"Całkowity tonaż dzisiejszej sesji to: {dzisiejszy_trening.calculate_total_volume()} kg")
 
 # Tworzymy nowy pomiar
-dzisiejsza_waga = BodyWeightLog("2026-07-03", 92.5, "Redukcja idzie zgodnie z planem")
+#dzisiejsza_waga = BodyWeightLog("2026-07-03", 92.5, "Redukcja idzie zgodnie z planem")
 
 # Wyświetlamy i zapisujemy
-dzisiejsza_waga.show_info()
-dzisiejsza_waga.save_to_json()
+#dzisiejsza_waga.show_info()
+#dzisiejsza_waga.save_to_json()
+
+# --- ODCZYT DANYCH ---
+
+def load_weight_log(date):
+    # 1. Budujemy ścieżkę do pliku, którego szukamy
+        filepath = os.path.join("logs", f"waga_{date}.json")
+    # 2. Zabezpieczenie: sprawdzamy, czy taki plik w ogóle istnieje
+        if os.path.exists(filepath):
+            #Otwieramy plik w trybie odczytu (r)
+            with open(filepath, 'r', encoding='utf-8') as file:
+                # Magia: Python zamienia tekst z pliku z powrotem na słownik
+                data = json.load(file)
+                print(f"Super! W dniu {data['date']} Twoja waga wynosiła: {data['weight']} kg.")
+                return data
+        else:
+            print(f"Błąd nie odnaleziono zapisu z dnia {date}.")
+            return None
+
+# Testujemy czytnik:
+# Wpisz tutaj dzisiejszą datę, pod którą zapisał się Twój plik z wagą
+# wczytane_dane = load_weight_log("2026-07-03")            
+
+def main_menu():
+    while True:
+        print("\n" + "="*35)
+        print("DZIENNIK TRENINGOWY MENU")
+        print("="*35)
+        print("1. Zapisz pomiar wagi")
+        print("2. Dodaj nowy trening")
+        print("3. Wyjście z programu")
+        print("="*35)
+
+        wybor = input("Wybierz opcję 1-3:")
+
+        if wybor == "1":
+            print("\n ---Dodawanie wagi--- ")
+            data = input("Podaj datę: np. (2026-07-06)")
+            try:
+                # Zamieniamy tekst z klawiatury na liczbę zmiennoprzecinkową (float)
+                waga = float(input("Podaj wagę w kg" ))
+                notatki = input("Dodatkowe notatki: (opcjonalne)")
+
+                nowy_pomiar = BodyWeightLog(data, waga, notatki)
+                nowy_pomiar.save_to_json()
+            except ValueError:
+                print("Błąd. Waga musi być liczbą. (np. 85)")
+
+        elif wybor == "2":
+            print("\n ---Dodawanie nowego treningu--- ")
+            data = input("Podaj datę treningu: np(2026-07-06)")
+            partia = input("Podaj trenowaną partię: np(Klatka i plecy)")
+
+            nowy_trening = Workout(data,partia)
+
+            # Wewnętrzna pętla do dodawania kolejnych serii
+            while True:
+                cwiczenie = input("\n Podaj nazwę ćwiczenia (lub wpisz 'koniec' aby zapisać trening): ")
+                # Jeśli wpiszesz 'koniec', przerywamy dodawanie serii
+                if cwiczenie.lower() == 'koniec':
+                    break
+
+                try:
+                    ciezar = float(input("Podaj ciężar roboczy w kg: "))
+                    powtorzenia = int(input("Podaj liczbę powtórzeń: "))
+
+                    nowa_seria = TrainingSet(cwiczenie, ciezar, powtorzenia)
+                    nowy_trening.add_set(nowa_seria)
+                except ValueError:
+                    print("Błąd. Ciężar i powtórzenia muszą być liczbami!")
+
+            # Gdy wyjdziemy z pętli wpisywania serii, zapisujemy cały trening
+            nowy_trening.save_to_json()
+
+        elif wybor == "3":
+            print("\n Zamykam, do zobaczenia na treningu. ")
+            break # To słowo kluczowe ostatecznie przerywa główną pętlę
+
+        else:
+            print("/n Nieznana opcja. Wybierz 1, 2 lub 3. ")
+
+# To jest standardowy sposób odpalania głównej funkcji w Pythonie
+if __name__ == "__main__":
+    main_menu()
+
+
+
+
+        
+
+
+
+              
